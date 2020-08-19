d66b3b9b09f61c63f78a6edc86e76d520cbb6aa8
esennesh <elisennesh@gmail.com>
2020-08-18 22:22:21 -0400

    util: refactor to separate softmaxing unbatched from batched logits
    Signed-off-by: esennesh <elisennesh@gmail.com>

--- discopyro/util.py
+++ discopyro/util.py
@@ -2,12 +2,15 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def softmax_logprobs(logprobs, dim1=-2, dim2=-1):
+def softmax_logits(logits, dim=-1):
+    nonzeros = logits.nonzero(as_tuple=True)
+    return torch.index_put(logits, nonzeros, F.softmax(logits[nonzeros],
+                                                       dim=dim))
+
+def batch_softmax(logprobs, dim1=-2, dim2=-1):
     logprobs = list(logprobs.unbind(dim=dim1))
     for i, logprob in enumerate(logprobs):
-        nonzeros = logprob.nonzero(as_tuple=True)
-        logprobs[i] = torch.index_put(logprob, nonzeros,
-                                      F.softmax(logprob[nonzeros], dim=dim2))
+        logprobs[i] = softmax_logits(logprob, dim2)
     return torch.stack(logprobs, dim=dim1)
 
 def optional_log(tensor):
@@ -18,8 +21,10 @@ def optional_energies(probs):
     return -optional_log(probs)
 
 def soften_logprobs(energies, temperature, dim1=-2, dim2=-1):
-    return softmax_logprobs(energies / temperature, dim1, dim2)
+    if dim2 is None:
+        return softmax_logits(energies / temperature, dim1)
+    return batch_softmax(energies / temperature, dim1, dim2)
 
 def soften_probabilities(probs, temperature, dim1=-2, dim2=-1):
     logprobs = optional_log(probs)
-    return soften_logprobs(logprobs, temperature + 1e-5, dim1, dim2)
+    return soften_logprobs(logprobs, temperature + 1e-10, dim1, dim2)
