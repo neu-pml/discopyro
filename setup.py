1b2142e8a0addd006c021645f3b06478baeabe2d
esennesh <elisennesh@gmail.com>
2020-11-16 16:38:10 -0500

    setup: increment minor version number
    Signed-off-by: esennesh <elisennesh@gmail.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="discopyro-bpl",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
 
     # Project uses reStructuredText, so ensure that the docutils get
