5d289d0a7a67560a784548b07a7a673277d4a675
esennesh <elisennesh@gmail.com>
2020-11-16 16:35:28 -0500

    discopyro.callable: monkey-patch biclosed.Diagram with a __call__ method
    Signed-off-by: esennesh <elisennesh@gmail.com>

--- discopyro/callable.py
+++ discopyro/callable.py
@@ -10,9 +10,7 @@ _PYTHON_FUNCTOR = PythonFunctor(
     ar=lambda f: Function(len(f.dom), len(f.cod), f.function)
 )
 
-class CallableDiagram(Diagram):
-    def __call__(self, *values):
-        return _PYTHON_FUNCTOR(self)(*values)
+Diagram.__call__ = lambda self, *values: _PYTHON_FUNCTOR(self)(*values)
 
 class CallableBox(Box, CallableDiagram):
     def __init__(self, name, dom, cod, function=None, data=None, _dagger=False):
